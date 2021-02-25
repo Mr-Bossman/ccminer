8db817b20cca53f7968050d35804540591c12b03
jesse <jesse@johnnys-MacBook-Pro.local>
2021-02-24 19:15:51 -0500

    tests

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ make distclean || echo clean
 rm -f Makefile.in
 rm -f config.status
 ./autogen.sh || echo done
-
+test=`pwd`
 # CFLAGS="-O2" ./configure
-./configure.sh
+./configure --host=arm64-apple-darwin --target=arm64-apple-darwin CC="xcrun -sdk iphoneos gcc -arch arm64" CXX="xcrun -sdk iphoneos g++ -arch arm64" LDFLAGS="-L$test"
 
 make
