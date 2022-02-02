52dde50c48db1de716efa1ef3c60f78c6e0a1c8e
Jesse Taube <Mr.Bossman075@gmail.com>
2022-02-01 22:51:13 -0500

    build

--- build.sh
+++ build.sh
@@ -1,16 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Simple script to create the Makefile and build
-
-# export PATH="$PATH:/usr/local/cuda/bin/"
+git submodule update --init
 
 make distclean || echo clean
 
 rm -f Makefile.in
 rm -f config.status
-./autogen.sh || echo done
 
-# CFLAGS="-O2" ./configure
-./configure.sh
+aclocal && autoheader && automake --add-missing --gnu --copy && autoconf
 
-make -j 4
+extracflags="-ffast-math -Ofast -funroll-loops -finline-functions -falign-functions=16 -falign-jumps=16 -falign-labels=16 -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align  -fvariable-expansion-in-unroller -fomit-frame-pointer -fpic -pthread -flto"
+
+export CXXFLAGS="$extracflags"
+export CPPFLAGS="$extracflags"
+export CFLAGS="$extracflags"
+export LDFLAGS="-v -flto  -Wl,-hugetlbfs-align"
+
+PATH="$PATH:/usr/local/cuda/bin/:/usr/local/opt/openssl/" ./configure CXX=clang++ CPP=clang-cpp CC=clang --with-no-cuda
+
+make -j $(nproc)
