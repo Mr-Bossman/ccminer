bbb347e753bbfcbfdd23f7fbcb7176db6638558b
Jesse Taube <Mr.Bossman075@gmail.com>
2022-02-02 20:01:35 -0500

    builds all deps

--- build.sh
+++ build.sh
@@ -1,22 +1,40 @@
 #!/usr/bin/env bash
 
 # Simple script to create the Makefile and build
-git submodule update --init
+git submodule update --init --recursive
 
 make distclean || echo clean
 
 rm -f Makefile.in
 rm -f config.status
+aclocal && autoheader && automake --add-missing --copy && autoconf
 
-aclocal && autoheader && automake --add-missing --gnu --copy && autoconf
-
-extracflags="-ffast-math -Ofast -funroll-loops -finline-functions -falign-functions=16 -falign-jumps=16 -falign-labels=16 -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align  -fvariable-expansion-in-unroller -fomit-frame-pointer -fpic -pthread -flto"
-
-export CXXFLAGS="$extracflags"
-export CPPFLAGS="$extracflags"
-export CFLAGS="$extracflags"
-export LDFLAGS="-v -flto  -Wl,-hugetlbfs-align"
-
-PATH="$PATH:/usr/local/cuda/bin/:/usr/local/opt/openssl/" ./configure CXX=clang++ CPP=clang-cpp CC=clang --with-no-cuda
-
+cd zlib
+./configure
+cd ../openssl
+./Configure
+if [[ "$(uname -s)" == *"Linux"* ]]; then
 make -j $(nproc)
+else
+make
+fi
+cd ../jansson
+aclocal && autoheader && automake --add-missing --copy && autoconf
+./configure
+cd ../curl
+./buildconf
+./configure  --enable-shared=no LDFLAGS="-L`cd ..;pwd`/openssl/" --with-zlib=`cd ..;pwd`/zlib --with-openssl=`cd ..;pwd`/openssl
+cd ..
+
+extracflags="-O3 -ffast-math -Ofast -funroll-loops -finline-functions -falign-functions=16 -falign-jumps=16 -falign-labels=16 -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -fvariable-expansion-in-unroller -fomit-frame-pointer -fpic -pthread -flto"
+if command -v clang &> /dev/null
+then
+CXX=clang++
+CC=clang
+fi
+PATH="$PATH:/usr/local/cuda/bin/" ./configure --with-no-cuda
+if [[ "$(uname -s)" == *"Linux"* ]]; then
+make -j $(nproc)
+else
+make
+fi
