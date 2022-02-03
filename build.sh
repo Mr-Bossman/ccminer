9d22b404bd11d4edabb5ceb0c8a60b31387074dd
Jesse Taube <Mr.Bossman075@gmail.com>
2022-02-02 20:01:35 -0500

    mac

--- build.sh
+++ build.sh
@@ -10,13 +10,18 @@ rm -f config.status
 
 aclocal && autoheader && automake --add-missing --gnu --copy && autoconf
 
-extracflags="-ffast-math -Ofast -funroll-loops -finline-functions -falign-functions=16 -falign-jumps=16 -falign-labels=16 -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align  -fvariable-expansion-in-unroller -fomit-frame-pointer -fpic -pthread -flto"
+extracflags="-ffast-math -Ofast -funroll-loops -finline-functions -falign-functions=16 -falign-jumps=16 -falign-labels=16 -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -fvariable-expansion-in-unroller -fomit-frame-pointer -fpic -pthread -flto"
 
+export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig"
 export CXXFLAGS="$extracflags"
-export CPPFLAGS="$extracflags"
+export CPPFLAGS="$extracflags -I/usr/local/opt/openssl/include"
 export CFLAGS="$extracflags"
-export LDFLAGS="-v -flto  -Wl,-hugetlbfs-align"
+export LDFLAGS="-v -flto -L/usr/local/opt/openssl/lib"
 
-PATH="$PATH:/usr/local/cuda/bin/:/usr/local/opt/openssl/" ./configure CXX=clang++ CPP=clang-cpp CC=clang --with-no-cuda
+PATH="$PATH:/usr/local/cuda/bin/" ./configure CXX=clang++ CC=clang --with-no-cuda
 
+if [[ "$(uname -s)" == *"Linux"* ]]; then
 make -j $(nproc)
+else
+make
+fi
